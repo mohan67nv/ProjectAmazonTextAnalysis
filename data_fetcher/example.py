48de4eea534883b5b13e15e9c5a9325c60e9b8a1
Mikkel Nylend <mikkelsn@stud.ntnu.no>
2016-12-08 23:07:38 +0100

    Splitted into training data and test data

--- data_fetcher/example.py
+++ data_fetcher/example.py
@@ -10,17 +10,40 @@ def parse(path):
         yield eval(l)
 
 
-def getDF(path):
+def get_training_data(path):
+    """
+    Get all usable data
+    :param path: path to compressed data
+    :return: panda data frame
+    """
     i = 0
     df = {}
     for d in parse(path):
-        df[i] = d
         i += 1
-        if (i+1) % 1000 == 0:
-            print("Step:", i)
+        if i <= 1400000:
+            df[i] = d
+        else:
+            break
+        if (i + 1) % 1000 == 0:
+            print("Step:", i + 1)
     return pd.DataFrame.from_dict(df, orient='index')
 
-start_time = time.time()
-df = getDF('D:/amazon_data/reviews_Electronics_5.json.gz')
 
-print("Time :", time.time()-start_time)
+# def get_test_data(path):
+#     """
+#     Do not call this before the real test!!!!
+#     """
+#     pass
+#     i = 0
+#     df = {}
+#     for d in parse(path):
+#         i += 1
+#         if i > 1400000:
+#             df[i] = d
+#     return pd.DataFrame.from_dict(df, orient='index')
+
+
+start_time = time.time()
+df = get_training_data('D:/amazon_data/reviews_Electronics_5.json.gz')
+
+print("Time :", time.time() - start_time)
